ab93967575295dce961274a68c52171e6770e7df
yarik <yaroslav.sotnikov.05@bk.ru>
2021-11-24 18:48:58 +0300

    new init

---
+++ main.py
@@ -0,0 +1,38 @@
+import sys
+from random import randrange
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.do_paint = False
+        self.initUI()
+
+    def initUI(self):
+        uic.loadUi('UI.ui', self)
+        self.btn.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def draw_flag(self, qp):
+        qp.setBrush(QColor(randrange(255), randrange(255), randrange(255)))
+        qp.drawEllipse(350, 260, randrange(200), randrange(200))
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
